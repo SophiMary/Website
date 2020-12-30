0a87683b4cb420ce9d2a1a5051871dd0fb0056a1
Sophie Mary <sophiemary@Phoenix.local>
2020-12-30 16:46:57 +0530

    add background image

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,33 @@
 import React from "react";
-import "./Home.css"
+import { makeStyles } from "@material-ui/core/styles";
+import productBG from "./Background.png";
+import productDownBG from "./CreatorBackground.png";
+import "./Home.css";
+
+const useStyles = makeStyles(theme => ({
+  page: {
+    background: `url(${productBG}), url(${productDownBG})`,
+    backgroundPosition: "50% -350%, 36% 190%",
+    height: "95vh",
+    padding: "15px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    fontSize: "32px",
+    textAlign: "center",
+    [theme.breakpoints.down("md")]: {
+      backgroundPosition: "50% -330%, 34% 183%",
+    },
+    [theme.breakpoints.down("sm")]: {
+      backgroundPosition: "20% -63%, 21% 221%",
+    },
+  },
+}));
 
 const Home = props => {
+  const classes = useStyles();
   return (
-  <div className="page">{props.info} Page</div>
+    <div className={classes.page}>{props.info} Page</div>
   );
 };
 
